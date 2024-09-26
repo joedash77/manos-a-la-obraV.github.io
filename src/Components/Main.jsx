b3bb3fe6f81fc375e9bf17eb5d9060d03e5d6711
Joel <joedash77@gmail.com>
2024-09-26 14:28:13 -0300

    agregue-routeo

---
+++ src/Components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Main.module.css';
+function Main() {
+  return (
+    <div className='main'>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Main
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Components/Main.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main/>
+  }
+])
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 )
