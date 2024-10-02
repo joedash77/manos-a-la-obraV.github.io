cb051a9ae067ba166cbedab8b8fcb2e0f2c2b747
Joel <joedash77@gmail.com>
2024-10-02 16:11:23 -0300

    caca

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,5 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-<<<<<<< HEAD
-=======
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Main from './Components/Main.jsx';
->>>>>>> b3bb3fe6f81fc375e9bf17eb5d9060d03e5d6711
 
 const router = createBrowserRouter([
   {
