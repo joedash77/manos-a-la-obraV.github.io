5e0d08dd4d073de7a78b69eff7179f2fda4497a5
Esteban <Estebaneqstek@gmail.com>
2024-10-12 13:10:06 -0300

    estilos v1.4

--- src/Components/paginas/Story.jsx
+++ src/Components/paginas/Story.jsx
@@ -2,28 +2,33 @@ import React from 'react'
 import Header from '../organisms/header/Header'
 import { dataList } from '../../Data/data'
 import { Link } from 'react-router-dom'
+import './StylesStory.css'
 
 function Story() {
   const projectList = dataList.map(project => (
-    <div key={project.id}>
+    <div className="contenedor-lista" key={project.id}>
       {project.epics.map(epic => (
-        <ul key={epic.id}>
+        <ul className="contenedor-story" key={epic.id}>
           {epic.stories.map(story => (
-            <li key={story.id}>
-              <Link to = {`/my-projects/${project.id}/epic/${epic.id}/story/${story.id}`} >{story.name}</Link>
+            <li className="historia" key={story.id}>
+              <Link to={`/my-projects/${project.id}/epic/${epic.id}/story/${story.id}`} className="story-link">
+                {story.name}
+              </Link>
             </li>
           ))}
         </ul>
       ))}
     </div>
-  ))
+  ));
 
   return (
-    <div>
-      <Header title='Mis historias' level = {2}/>
-      {projectList}
-    </div>
-  )
+    <>
+      <Header title='Mis historias' level={2} />
+      <div className="story-list">
+        {projectList}
+      </div>
+    </>
+  );
 }
 
 export default Story
