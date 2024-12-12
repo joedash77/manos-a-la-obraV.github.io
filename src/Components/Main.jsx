7df4aee3f6d314ea98fdc902c31e65c41623a02f
Joel <joedash77@gmail.com>
2024-12-12 16:39:34 -0300

    aac

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -6,22 +6,29 @@ import { useFetchResource } from '../utils/apiCalls';
 
 function Main() {
   const userId = localStorage.getItem('userID');
+
   const { resource: user } = useFetchResource(`users/${userId}`);
   const { resource: project } = useFetchResource(`projects/user/${userId}`);
+  const { resource: stories } = useFetchResource(`stories/user/${userId}`);
 
   const [totalProjects, setTotalProjects] = useState(0);
+  const [totalStories, setTotalStories] = useState(0);
 
   useEffect(() => {
     if (project) {
       setTotalProjects(project.length); // Calcula el total de proyectos
     }
-  }, [project]); 
+    if (stories){
+      setTotalStories(stories.length);
+    }
+  }, [project, stories]); 
 
   return (
     <div>
         <Header title='Menu'/>
         <h1>Bienvenido {user.username}</h1>
         <h2>Proyectos creados: {totalProjects}</h2>
+        <h2>Historias creadas: {totalStories}</h2>
     </div>
   )
 }
